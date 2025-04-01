4aeb77ca31295aaa567eab108d441d468cdc3c81
raymocao <raymondcao2002@gmail.com>
2025-03-31 21:49:31 -0500

    add confirm cancel

--- booget/hud.gd
+++ booget/hud.gd
@@ -4,21 +4,37 @@ signal add_transaction(transaction);
 signal end_game();
 
 func _ready():
-	$MarginContainer/VBoxContainer/LB.show();
-	$MarginContainer/VBoxContainer/FB.show();
-	$MarginContainer/"Add Transaction".show();
+	load_main_screen();
+
+func load_main_screen():
+	$MainMarginContainer.show();
+	$MainMarginContainer/BudgetLabels/LB.show();
+	$MainMarginContainer/BudgetLabels/FB.show();
+	$MainMarginContainer/BudgetLabels/FP.show();
+	$MainMarginContainer/"Add Transaction".show();
+	$ExitMarginContainer.hide();
 
 func update_lb(num):
-	$MarginContainer/VBoxContainer/LB.text = "Living Budget Remaining: " + str(num);
+	$MainMarginContainer/BudgetLabels/LB.text = "Living Budget Remaining: " + str(num);
 
 func update_fb(num):
-	$MarginContainer/VBoxContainer/FB.text = "Fun Budget Remaining: " + str(num);
+	$MainMarginContainer/BudgetLabels/FB.text = "Fun Budget Remaining: " + str(num);
+
+func update_fp(num):
+	$MainMarginContainer/BudgetLabels/FP.text = "Fun Pool: " + str(num);
 
 func _on_add_transaction_pressed():
 	var transaction = Transaction.new();
-	transaction.transaction_amount = 10;
+	transaction.transaction_amount = 550;
 	transaction.transaction_category = Main.BudgetCategory.LIVING;
 	add_transaction.emit(transaction);
 
 func _on_exit_pressed() -> void:
+	$MainMarginContainer.hide();
+	$ExitMarginContainer.show();
+
+func _on_exit_confirm_pressed() -> void:
 	end_game.emit();
+
+func _on_exit_cancel_pressed() -> void:
+	load_main_screen();
